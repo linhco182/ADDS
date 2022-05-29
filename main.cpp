d0ca170ec54dae500b96b77f4821ac93869d33fe
linhco182 <72375675+linhco182@users.noreply.github.com>
2022-05-30 01:40:20 +0930

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,102 +1,46 @@
-#include "Node.h"
 #include <iostream>
-#include "LinkedList.h"
 #include <vector>
-
+#include <string>
+#include "Calculator.h"
 
 int main(){
-    std::string input = "5 2 7 10 AP 3 9";
-    std::array<int,100> numArray;
-    std::vector<int> params;
-    std::string code;
-    std::string number;
-    int sLength = input.length();
-    int Pos;
-    int index = 0;
-    for(int i = 0; i < input.length();i++){
-        if(isalpha(input[i])){
-            code.push_back(input[i]);
-            Pos = i;
-        }
-    }
-    int pointA = 0;
-    for (int i = 0; i < sLength; i++ ){
 
-
-        if (input[i]==' '){
-            for(int j = pointA; j < i; j++){
-                number.push_back(input[j]);
-                
+    std::string input;
+    std::getline(std::cin,input);
+    int numPos=0;
+    std::vector<std::string> vectorInput;
+    std::string word;
+    for(int i = numPos; i < input.length(); i++){
+        if(input[i]==(' ')){
+            if(numPos==0){
+                numPos=-1;
             }
-            pointA = i;
+            for(int j = numPos+1; j < i; j++){
+                word.push_back(input[j]);
 
-            numArray[index]=stoi(number);
-            index++;
-            number.clear();
-        }
-        if(isalpha(input[i])){
-            break;
-        }
-
-    }
-    pointA = 0;
-    for(int i = Pos+1; i< sLength; i++){
-        if (input[i]==' '){
-            for(int j = pointA; j < i; j++){
-                number.push_back(input[j]);
-                
             }
-            pointA = i;
-            params.push_back(stoi(number));
-            number.clear();
+            vectorInput.push_back(word);
+            word.clear();
+            numPos=i;
+            
+        }
+        
+
+        if(i==input.length()-1){
+            for(int j = numPos+1; j < input.length(); j++){
+                word.push_back(input[j]);
+
+            }
+            vectorInput.push_back(word);
+            word.clear();
+            numPos=i;
         }
     }
-    for(int i = pointA; i< sLength; i++){
-        number.push_back(input[i]);
-    }
-    params.push_back(stoi(number));
-            number.clear();
 
-
-
-
-    std::cout<< code<<std::endl;
-
-
-    int * ptr = &numArray[0];
-    LinkedList list1 = LinkedList(ptr,index);
-
-    if(code=="AF"){
-        list1.addFront(params[1]);
-    }
-    if(code=="AE"){
-        list1.addEnd(params[1]);
-    }
-    if(code=="AP"){
-        list1.addAtPosition(params[1],params[2]);
-    }
-    if(code=="S"){
-        list1.search(params[1]);
-    }
-    if(code=="DF"){
-        list1.deleteFront();
-    }
-    if(code=="DE"){
-        list1.deleteEnd();
-    }
-    if(code=="DP"){
-        list1.deletePosition(params[1]);
-    }
-    if(code=="GI"){
-        list1.getItem(params[1]);
-    }
-
-    list1.printItems();
-
-
-
-
-
+    Calculator calc = Calculator();
+    calc.evaluate(vectorInput);
+    std::cout<<calc.getExpression();
+    
 
     return 0;
 }
